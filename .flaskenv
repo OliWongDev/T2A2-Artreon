284fbcb9a92ce4c63032cbaac982241df0ed8fcb
OliWongDev <14wongom@gmail.com>
2022-11-06 23:32:57 +1100

    Postgres errors part fix

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 FLASK_APP=main:create_app
 FLASK_DEBUG=true
+FLASK_ENV=development
--- schemas/artist_schema.py
+++ schemas/artist_schema.py
@@ -2,6 +2,6 @@ from main import ma
 
 class ArtistSchema(ma.Schema):
     class Meta:
-        fields = ("id", "password", "email", "is_admin", "artist_bio")
+        fields = ("id", "artreon_alias", "password", "email", "is_admin", "artist_bio")
 
 artist_schema = ArtistSchema()
